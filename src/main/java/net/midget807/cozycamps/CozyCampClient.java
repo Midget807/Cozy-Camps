f15c204869d5bce402eb3642631882cc037fec37
Midget807 <phu7mai@gmail.com>
2026-05-31 21:31:06 +0930

    Initial commit

---
+++ src/main/java/net/midget807/cozycamps/CozyCampClient.java
@@ -0,0 +1,10 @@
+package net.midget807.cozycamps;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CozyCampClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/midget807/cozycamps/CozyCampsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.midget807.cozycamps;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CozyCampsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
